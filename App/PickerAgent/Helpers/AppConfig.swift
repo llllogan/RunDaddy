507548bda1ec1b9c540660c3bb08c55d12f10497
Logan <logan.janssen@icloud.com>
2025-12-16 21:26:53 +1000

    Updated api url

--- App/PickerAgent/Helpers/AppConfig.swift
+++ App/PickerAgent/Helpers/AppConfig.swift
@@ -10,8 +10,8 @@ import Foundation
 enum AppConfig {
     /// Base URL for all RunDaddy API requests.
     static let apiBaseURL: URL = {
-//        return URL(string: "https://pickeragent.app/api")!
-      return URL(string: "https://rundaddy.app/api")!
+        return URL(string: "https://pickeragent.app/api")!
+//      return URL(string: "https://rundaddy.app/api")!
 //      return URL(string: "http://localhost:3000/api")!
     }()
 }
